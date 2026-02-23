42dc95857aa6e6b9af31ed0260cf8a2ed72a62b5
ps0949 <ps0949@srmist.edu.in>
2026-02-23 19:00:43 +0530

    uc2:Display OOPS as 7-line ASCII banner

--- Oopsbannerapp.java
+++ Oopsbannerapp.java
@@ -1,6 +1,11 @@
-public class Oopsbannerapp{
-    public static void main(String[] args)
-{
-  System.out.println("OOPS");   
-}
+public class Oopsbannerapp {
+    public static void main(String[] args) {
+        System.out.println("  ***    ***    *****   ***** ");
+        System.out.println(" *   *  *   *   *    *  *     ");
+        System.out.println("*     **     *  *    *  *     ");
+        System.out.println("*     **     *  *****   ***** ");
+        System.out.println("*     **     *  *           * ");
+        System.out.println(" *   *  *   *   *           * ");
+        System.out.println("  ***    ***    *       ***** ");
+    }
 }
